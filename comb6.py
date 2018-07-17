8bd68cbccc65c4e5d2dee3863888c5fc927c468c
Evan Ranken <eranken@ur.rochester.edu>
2018-07-17 09:06:39 -0500

    67

---
+++ comb6.py
@@ -0,0 +1,20 @@
+import argparse,os
+import sys
+from subprocess import call
+
+#comment
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--o',action='store', type=str, default = "diffNuisances.root")
+parser.add_argument('--M',action='store', type=str, default = "MaxLikelihoodFit")
+parser.add_argument('--t',action='store', type=str, default = "-1")
+parser.add_argument('--freeze',action='store', type=str, default = "r")
+parser.add_argument('--set',action='store', type=str, default = "r=1")
+parser.add_argument('--poi',action='store', type=str, default = "gt")
+parser.add_argument('--opt',action='store', type=str, default = "")
+
+args = parser.parse_args()
+
+os.system('cmsenv')
+os.system('combine -M '+args.M+' -d card.root --redefineSignalPOIs '+args.poi+' --freezeParameters '+args.freeze+' --setParameters '+args.set+' -t '+args.t+' '+args.opt)
+os.system('python ../../diffNuisances.py --poi '+args.poi+' -a fitDiagnostics.root -g '+args.o)
---
+++ impacts7.py
@@ -0,0 +1,24 @@
+import argparse,os
+import sys
+from subprocess import call
+
+#comment
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--o',action='store', type=str, default = "impacts")
+parser.add_argument('--M',action='store', type=str, default = "MaxLikelihoodFit")
+parser.add_argument('--t',action='store', type=str, default = "-1")
+parser.add_argument('--freeze',action='store', type=str, default = "r")
+parser.add_argument('--set',action='store', type=str, default = "r=1")
+parser.add_argument('--poi',action='store', type=str, default = "gt")
+parser.add_argument('--opt',action='store', type=str, default = "")
+
+args = parser.parse_args()
+
+#os.system('cmsenv')
+
+os.system('combineTool.py -M Impacts -d card.root -m 125 --redefineSignalPOIs '+args.poi+' --freezeParameters '+args.freeze+' --setParameters '+args.set+' --doInitialFit --robustFit 1 -t '+args.t)
+os.system('combineTool.py -M Impacts -d card.root -m 125 --redefineSignalPOIs '+args.poi+' --freezeParameters '+args.freeze+' --setParameters '+args.set+' --doFits --robustFit 1 -t '+args.t)
+os.system('combineTool.py -M Impacts -d card.root -m 125 --redefineSignalPOIs '+args.poi+' --freezeParameters '+args.freeze+' --setParameters '+args.set+' -o'+args.o+'.json')
+os.system('plotImpacts.py -i '+args.o+'.json -o '+args.o)
+#os.system('gitup '+args.o+'.pdf')
