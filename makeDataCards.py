59e113207a25b57f282f73b13555004fdd2a3e7c
eranken <eranken@gmail.com>
2019-02-14 12:28:59 +0100

    updated +makeDataCards.py

--- makeDataCards.py
+++ makeDataCards.py
@@ -11,43 +11,47 @@ process = [
 		"st",
 		]
 
+syslist=['JES','JES_AbsoluteScale','JES_PileUpPtRef','JES_FlavorQCD','flatsys','mtop','JES_RelativeFSR','JES_RelativePtEC2','pdf0','JES_RelativePtEC1','elrec','rs','eltrg','JES_AbsoluteMPFBias','mutrg','murec','JER','JES_Fragmentation','fs','bfrag','JES_SinglePionHCAL','bdec','JES_RelativePtBB','JES_RelativeSample','JES_PileUpPtEC1','JES_RelativeBal','pdf2','pdf3','JES_PileUpDataMC','pdf1','pdf7','pdf4','pdf5','alphas','JES_SinglePionECAL','btag','isr','pu','JES_RelativeJEREC1','ltag','fsr','JES_PileUpPtBB','JES_TimePtEta',]
+
+
 systematics = [
 		#lnNSystematic("flat"	,["ttsig"]	,[1.03]),
 		#lnNSystematic("hd"	,["ttsig"]	,[1.0152]),
 		#lnNSystematic("tune"	,["ttsig"]	,[1.0074]),
-		lnNSystematic("lumi"	,["ttsig","ttbg","vj","st"]	,[1.023,1.023,1.023,1.023]),
+		lnNSystematic("lumi"	,["ttsig","ttbg","vj","st"]	,[1.023,1.023,1.023,1.023])
 		#lnNSystematic("ltag"	,["ttsig"]	,[1.0015]),
 		#lnNSystematic("isr"	,["ttsig"]	,[1.00145],),
-		
 		#ShapeSystematic("flat_shape"	,["ttsig"]),
-		
-		ShapeSystematic("ltag"	,["ttsig"]),
-		ShapeSystematic("btag"	,["ttsig"]),
-		ShapeSystematic("fsr"	,["ttsig"]),
-		ShapeSystematic("isr"	,["ttsig"]),
-		ShapeSystematic("fs"	,["ttsig"]),
-		ShapeSystematic("rs"	,["ttsig"]),
-		ShapeSystematic("JER"	,["ttsig"]),
-		ShapeSystematic("JES"	,["ttsig"]),
-		ShapeSystematic("hd"	,["ttsig"]),
-		ShapeSystematic("tune"	,["ttsig"]),
-		ShapeSystematic("pu"	,["ttsig"]),
-		ShapeSystematic("mtop"	,["ttsig"]),
-		ShapeSystematic("bfrag"	,["ttsig"]),
-		ShapeSystematic("bdec"	,["ttsig"]),
-		ShapeSystematic("elrec"	,["ttsig"]),
-		ShapeSystematic("eltrg"	,["ttsig"]),
-		ShapeSystematic("murec"	,["ttsig"]),
-		ShapeSystematic("mutrg"	,["ttsig"]),
-		ShapeSystematic("pdf0"	,["ttsig"]),
-		ShapeSystematic("pdf1"	,["ttsig"]),
-		ShapeSystematic("pdf2"	,["ttsig"]),
-		ShapeSystematic("pdf3"	,["ttsig"]),
-		ShapeSystematic("pdf4"	,["ttsig"]),
-		ShapeSystematic("pdf5"	,["ttsig"]),
-		ShapeSystematic("pdf6"	,["ttsig"]),
-		ShapeSystematic("alphas"	,["ttsig"]),
+		# ShapeSystematic("ltag"	,["ttsig"]),
+		# ShapeSystematic("btag"	,["ttsig"]),
+		# ShapeSystematic("fsr"	,["ttsig"]),
+		# ShapeSystematic("isr"	,["ttsig"]),
+		# ShapeSystematic("fs"	,["ttsig"]),
+		# ShapeSystematic("rs"	,["ttsig"]),
+		# ShapeSystematic("JER"	,["ttsig"]),
+		# ShapeSystematic("JES"	,["ttsig"]),
+		# ShapeSystematic("hd"	,["ttsig"]),
+		# ShapeSystematic("tune"	,["ttsig"]),
+		# ShapeSystematic("pu"	,["ttsig"]),
+		# ShapeSystematic("mtop"	,["ttsig"]),
+		# ShapeSystematic("bfrag"	,["ttsig"]),
+		# ShapeSystematic("bdec"	,["ttsig"]),
+		# ShapeSystematic("elrec"	,["ttsig"]),
+		# ShapeSystematic("eltrg"	,["ttsig"]),
+		# ShapeSystematic("murec"	,["ttsig"]),
+		# ShapeSystematic("mutrg"	,["ttsig"]),
+		# ShapeSystematic("pdf0"	,["ttsig"]),
+		# ShapeSystematic("pdf1"	,["ttsig"]),
+		# ShapeSystematic("pdf2"	,["ttsig"]),
+		# ShapeSystematic("pdf3"	,["ttsig"]),
+		# ShapeSystematic("pdf4"	,["ttsig"]),
+		# ShapeSystematic("pdf5"	,["ttsig"]),
+		# ShapeSystematic("pdf6"	,["ttsig"]),
+		# ShapeSystematic("alphas"	,["ttsig"]),
 		]
+
+for sys in syslist:
+	systematics.append(ShapeSystematic(sys,["ttsig"]))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--inDir',action='store')
