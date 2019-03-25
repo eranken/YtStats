f79a925738604ae7bba8c13253f308a1f74a5aad
eranken <eranken@gmail.com>
2019-03-25 16:50:28 +0100

    updated +makeDataCards.py

--- makeDataCards.py
+++ makeDataCards.py
@@ -68,7 +68,7 @@ for sys in syslist:
 	systematics.append(ShapeSystematic(sys,["ttsig"]))
 
 rootFileReader = RootFileReader()
-rootFileReader.readFile(option.inDir+'combine.root')
+rootFileReader.readFile(option.inDir+'combine_'+year+chan+'.root')
 binCollection = rootFileReader.createBinCollection(process,systematics)
 
 textFilePath = option.inDir+'combine_EW_'+year+chan+'.txt'
