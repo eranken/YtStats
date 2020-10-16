166aa13f4be6d1c764e708989bb9bba4a3668bb9
Evan Ranken <e.ranken@cern.ch>
2020-10-16 19:04:25 +0200

    updated +modImpacts.py

--- modImpacts.py
+++ modImpacts.py
@@ -407,13 +407,29 @@ for page in xrange(n):
 
 	# Back to the first pad to draw the pulls graph
 	pads[0].cd()
+
+		lo_color = {
+			'default': 38,
+			'hesse': ROOT.kOrange - 3,
+			'robust': ROOT.kGreen + 1
+		}
+		hi_color = {
+			'default': 46,
+			'hesse': ROOT.kBlue,
+			'robust': ROOT.kAzure - 5
+		}
+		method = 'default'
+		if 'method' in data and data['method'] in lo_color:
+			method = data['method']
+
 	plot.Set(g_pulls, MarkerSize=0.8, LineWidth=2)
 	# plot.Set(g_pullsA, MarkerSize=0.8, LineWidth=2)
 	g_pulls.Draw('PSAME')
 	# g_pullsA.SetFillColor(plot.CreateTransparentColor(ROOT.kBlack, .3))
 	# g_pullsA.SetFillColor(ROOT.kRed)
 	g_pullsA.SetFillStyle(1)
-	g_pullsA.SetFillColor(plot.CreateTransparentColor(ROOT.kRed, 0.5))
+	g_pullsA.SetLineWidth(0)
+	g_pullsA.SetFillColor(plot.CreateTransparentColor(hi_color[method], alpha))
 	g_pullsA.Draw('2SAME')
 
 	# And back to the second pad to draw the impacts graphs
